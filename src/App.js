6cf4dfb24956556992690eaaeca25f2d890d6f57
RicardoVeronica <charlie.bronson00@gmail.com>
2021-07-05 00:38:44 -0500

    adds Form component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Form from './componets/Form'
+
 function App() {
   return (
     <>
       <h1>Hello world</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Form />
+          </div>
+          <div className="one-half column">
+            <h1>Second column</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/componets/Form.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react"
+
+function Form(){
+  return(
+    <>
+      <h1>Form</h1>
+    </>
+    )
+}
+
+export default Form

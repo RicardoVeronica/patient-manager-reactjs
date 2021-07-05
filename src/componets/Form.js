023a930ba9c0da7e4b34601538869f99ed5715be
RicardoVeronica <charlie.bronson00@gmail.com>
2021-07-05 00:52:42 -0500

    adds form inputs

--- src/componets/Form.js
+++ src/componets/Form.js
@@ -3,7 +3,49 @@ import React, {useState} from "react"
 function Form(){
   return(
     <>
-      <h1>Form</h1>
+      <h2>Ingreso</h2>
+      <form>
+        <label htmlFor="name">Nombre del Paciente</label>
+        <input
+          className="u-full-width"
+          id="name"
+          type="text"
+          name="paciente"
+          placeholder="Nombre del paciente"
+        />
+        <label htmlFor="familiar">Nombre del Familiar</label>
+        <input
+          className="u-full-width"
+          id="familiar"
+          type="text"
+          name="familiar"
+          placeholder="Nombre del familiar"
+        />
+        <label htmlFor="date">Fecha de Ingreso</label>
+        <input
+          className="u-full-width"
+          id="date"
+          type="date"
+          name="data"
+        />
+        <label htmlFor="time">Hora de Ingreso</label>
+        <input
+          className="u-full-width"
+          id="time"
+          type="time"
+          name="time"
+        />
+        <label htmlFor="diagnosis">Diasnoctico</label>
+        <textarea
+          className="u-full-width"
+          id="diagnosis"
+          name="diagnosis"
+        ></textarea>
+        <button
+          className="u-full-widht button-primary"
+          type="submit"
+        >Agregar Paciente</button>
+      </form>
     </>
     )
 }
